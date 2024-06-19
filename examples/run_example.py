6dda4ca7208e92ab329a7b1e3494611a0ba0e9b7
Jakub Sido <JakubSido@gmail.com>
2024-06-19 18:52:52 +0200

    example updated

--- examples/run_example.py
+++ examples/run_example.py
@@ -1,49 +1,69 @@
-import itertools
-import socket
-import sys
+from datetime import datetime, timedelta
 from metarunner import Metarunner, MetarunnerArgs
 import os
 
 print("KERBEROS TICKETS: ", os.system("klist"))
 
-if __name__ == '__main__':
 
-    PROJECT_DIR = "/storage/brno1-cerit/home/sidoj/prompt"
-    MAIN_SCRIPT = "src/example.py"
-
-    # qsub -I -l walltime=24:0:0 -q gpu@meta-pbs.metacentrum.cz -l select=1:ncpus=1:ngpus=1:mem=40000mb:scratch_local=40000mb:cl_adan=True:mpiprocs=1:ompthreads=1
-    # singularity run --nv /cvmfs/singularity.metacentrum.cz/NGC/PyTorch\:21.03-py3.SIF  /storage/brno1-cerit/home/sidoj/phd_augmenter
-
-    def generate_plan_job(job_script,run_guid, run_seq_id, data_path):
+if __name__ == "__main__":
+    home_dir =  "/storage/plzen1/home/sidoj"
+    metarunner_root = f"{home_dir}/projects/sph_data_downloader"
+    time_str = "0:04:00"
+    time_obj = datetime.strptime(time_str, '%H:%M:%S')
+    total_seconds = timedelta(hours=time_obj.hour, minutes=time_obj.minute, seconds=time_obj.second).total_seconds()
+    seconds_for_data_bck = 2*60
+    
+    # PBS -q gpu@cerit-pbs.cerit-sc.cz
+    def plan_script(script_path, run_guid, run_seq_id, data_dir):
         return f"""#!/bin/bash
-                #PBS -q gpu@cerit-pbs.cerit-sc.cz
-                #P-BS -q gpu@meta-pbs.metacentrum.cz
-                #PBS -l walltime=0:59:0
-                #PBS -l select=1:ncpus=1:ngpus=1:mem=40000mb:scratch_local=40000mb:cl_zia=True
-                #P-BS -l select=1:ncpus=1:ngpus=1:mem=40000mb:scratch_local=40000mb:cl_galdor=True
-                #PBS -N augmenter-{run_guid}-{run_seq_id}
+                #PBS -q iti@pbs-m1.metacentrum.cz
+                #PBS -l select=1:ncpus=4:mem=40gb:scratch_local=40gb
 
-                /bin/bash {job_script}
+                #PBS -l walltime={time_str}
+                #PBS -N sph-d-{run_guid}-{run_seq_id}
+                
+                source ~/.bashrc   #for bck_folder init
+
+                cleanup(){{
+                    echo 'Running cleanup'
+                    bck_folder $SCRATCHDIR {data_dir}
+                    exit
+                }}
+
+                # timeout for enough time at the end of the job ({seconds_for_data_bck} seconds)
+                timeout -s SIGKILL {total_seconds-seconds_for_data_bck} /bin/bash -c 'HOME={home_dir} {script_path}'
+
+                echo "job was killed by me or just ending to cleanup"
+                cleanup
                 """
-
 
     def generate_run_job(named_args):
-        named_params = " ".join(f"--{k} {v}" for k, v in named_args.items())
+        db_file_name = os.path.basename(named_args['file'])
+        hdf5_file_name = f"{db_file_name.split('.')[0]}.hdf5"
+    
         return f"""#!/bin/bash
-                module add conda-modules
+                
+                
+                module add mambaforge
 
-                HOME=/storage/brno1-cerit/home/sidoj/
-                conda activate prompts
+                mamba create -p $SCRATCHDIR/sph_clone --clone /storage/plzen1/home/sidoj/tools/miniconda/envs/sphincter
+                conda activate $SCRATCHDIR/sph_clone
 
-                export TMPDIR=$SCRATCH
+                export TMPDIR=$SCRATCHDIR
 
-                cd {PROJECT_DIR}
-                export PYTHONPATH=$(pwd)
-                python {MAIN_SCRIPT} {named_params}
+                cd {metarunner_root}
+                cp {named_args['file']} $SCRATCHDIR/{db_file_name}
+            
+                python /storage/brno2/home/sidoj/projects/sph_data_downloader/src/download/sql_to_hdf5.py $SCRATCHDIR/{db_file_name} $SCRATCHDIR{hdf5_file_name}
+                rm $SCRATCHDIR/{db_file_name}
+
+                echo "job script ending..."        
                 """
 
+    mr = Metarunner(metarunner_root, plan_script, generate_run_job)
 
-    mr = Metarunner(PROJECT_DIR, generate_plan_job, generate_run_job)
+
+
     map_hp_vals = {
         "a": [
             5,
